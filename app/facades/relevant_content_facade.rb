7087aa74caa9288defe822fbe721bbb6cca8135d
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-28 12:34:53 -0600

    Update count attribute

--- app/facades/relevant_content_facade.rb
+++ app/facades/relevant_content_facade.rb
@@ -3,14 +3,14 @@ class RelevantContentFacade
 
   def initialize(params)
     @id = nil
-    @count = params[:count].to_i || 50
+    @count = params[:count] || 50
     @params = params
   end
 
   def videos
     youtube_service.videos[:items].map do |data|
       YoutubeVideo.new(data)
-    end.sample(count)
+    end.sample(count.to_i)
   end
 
   private
