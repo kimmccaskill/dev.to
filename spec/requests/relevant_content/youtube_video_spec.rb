bd0928741263142f95cb2cc7d84554f5c3f9465c
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-28 12:33:26 -0600

    Update specs

--- spec/requests/relevant_content/youtube_video_spec.rb
+++ spec/requests/relevant_content/youtube_video_spec.rb
@@ -1,11 +1,50 @@
 require "rails_helper"
 
-RSpec.describe "Relevant Content", :vcr do
-  describe "YouTube Video Request" do
-    it "can get a list of 50 youtube videos" do
-      keywords = "westworld robots sdagsjkhgskjdkjsdgsdg"
+RSpec.describe "Relevant Content" do
+  describe "YouTube Video Request", :vcr do
+    let(:start_time) { VCR.current_cassette&.originally_recorded_at || Time.current }
 
-      get "/relevant_content?keywords=#{keywords}&tags=webdev java career&count=50"
+    before do
+      $REDIS.flushall
+      Timecop.travel(start_time)
+    end
+
+    it "can get a list of 50 youtube videos" do
+      keywords = "westworld robots"
+
+      get "/relevant_content?keywords=#{keywords}"
+      expect(response).to be_successful
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+      expect(parsed_response[:data][:attributes][:videos].length).to eq 50
+    end
+
+    it "can get a list of videos from cache" do
+      keywords = "westworld robots"
+
+      get "/relevant_content?keywords=#{keywords}"
+      expect(response).to be_successful
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+      expect(parsed_response[:data][:attributes][:videos].length).to eq 50
+
+      get "/relevant_content?keywords=#{keywords}"
+      expect(response).to be_successful
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+      expect(parsed_response[:data][:attributes][:videos].length).to eq 50
+    end
+
+    it "can get a specified count of videos" do
+      keywords = "westworld robots"
+
+      get "/relevant_content?keywords=#{keywords}&count=5"
+      expect(response).to be_successful
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+      expect(parsed_response[:data][:attributes][:videos].length).to eq 5
+    end
+
+    it "can get a list of videos from tags if no keywords given" do
+      tags = "git, java, beginners, career"
+
+      get "/relevant_content?tags=#{tags}"
       expect(response).to be_successful
       parsed_response = JSON.parse(response.body, symbolize_names: true)
       expect(parsed_response[:data][:attributes][:videos].length).to eq 50
